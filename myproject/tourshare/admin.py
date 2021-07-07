95d7b7188014c7c1f9a1f027e02944fc91f3c73c
udoy <rafeuludoy@gmail.conm>
2021-07-07 19:34:27 +0600

    first commit

---
+++ myproject/tourshare/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import create_user,create_team,join_user,Room,Message
+
+admin.site.register(create_user)
+admin.site.register(create_team)
+admin.site.register(join_user)
+admin.site.register(Room)
+admin.site.register(Message)
---
+++ myproject/tourshare/urls.py
@@ -0,0 +1,23 @@
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.index, name="tourshare"),
+    path("aboutus/",views.about, name="AboutUs"),
+    path("createteam/",views.createteam, name="CreateTeam"),
+    path("checkout/",views.checkout, name="CheckOut"),
+    
+    
+    
+    path("jointeam/",views.jointeam, name="JoinTeam"),
+    path("signin/",views.signin, name="SignIn"),
+    path("signout/",views.signout, name="SignOut"),
+    
+    path("signup/",views.signup, name="Signup"),
+    path("contactus/",views.contactus, name="ContactUs"),
+    path("connect/",views.connect, name=" Connect"),
+    path('<str:room>/', views.room, name='room'),
+    path('checkview', views.checkview, name='checkview'),
+]
